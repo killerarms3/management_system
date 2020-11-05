70894da4b8908580ce012dc317ce72490bd61bb2
jackyhsu <jacku_hsu@genorx.com>
2020-11-05 10:50:29 +0800

    merge

--- management_system/settings.py
+++ management_system/settings.py
@@ -125,9 +125,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hant'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
@@ -157,6 +157,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'email@address'
-EMAIL_HOST_PASSWORD = 'password'
+EMAIL_HOST_USER = 'killerarms3@gmail.com'
+EMAIL_HOST_PASSWORD = 'DoNTBeaJoKE4064'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
